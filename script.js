064b98fed88728108eee61e54da87e2f74390477
Danny Roberts <dannyroberts.b@gmail.com>
2021-09-29 09:48:16 +0100

    fixed jquery for secondary accordions

--- script.js
+++ script.js
@@ -3,11 +3,28 @@ $(document).ready(function () {
   const $accordion = $(".accordion__individual");
 
   $($accordion).on("click", function () {
-    $(this).children(".accordion__individual--secondary").toggleClass("active");
-    $(this).find($arrowSelector).addClass("arrow-rotate");
-    $(this).find($arrowSelector).addClass("arrow-rotate-back");
-    setTimeout(() => {
-      $(this).find($arrowSelector).removeClass("arrow-rotate");
-    }, 600);
+    // $(this).children(".accordion__individual--secondary").toggleClass("active");
+    $(this).children(".accordion__individual--secondary").toggle(500);
+    $(this).find($arrowSelector).toggleClass("arrow-active");
+    // $(this).children(".accordion__individual--secondary").css({
+    //   display: "block",
+    //   transform: "translateY(100%)",
+    //   "transition-duration": "0.5s",
+    // });
+
+    if ($arrowSelector.hasClass("arrow-active")) {
+      $(this).find($arrowSelector).css({
+        transform: "rotate(90deg)",
+        "transition-duration": "0.5s",
+      });
+    } else {
+      $(this).find($arrowSelector).css({
+        transform: "rotate(-90deg)",
+        "transition-duration": "0.5s",
+      });
+    }
+    // setTimeout(() => {
+    //   $(this).find($arrowSelector).removeClass("arrow-rotate");
+    // }, 600);
   });
 });
